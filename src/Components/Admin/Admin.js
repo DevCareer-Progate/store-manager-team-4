61e7bbacbf7ae0a75c5a2904d3fd0c5cf8b6eb2f
ikiv-rach <victoriasalmi18@gmail.com>
2020-07-20 15:31:50 +0100

    feat/admin page

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import classes from "./Admin.module.css";
 
-const admin = props => <p>ADMIN PAGE!!!!!!!!!!!!!!!!</p>;
-export default admin;
+import passport from "../../assests/images/passport.jpg";
+
+import Button from "../Button/Button";
+
+const Admin = (props) => {
+  const data = ["New product", " New sales", "Total Products", "Total Sales"];
+
+  return (
+    <Fragment>
+      <div>
+        <img className={classes.profile} src={passport} />
+        <p>Edit profile</p>
+      </div>
+
+      <div className={classes.image}></div>
+
+      <div className={classes.list}>
+        {/* <list> */}
+        <ul>
+          <li>Name:</li>
+          <li>Username:</li>
+          <li>Staff ID:</li>
+          <li className={classes.border}></li>
+          <li>Total products added:</li>
+          <li>Total Sales made:</li>
+        </ul>
+
+        {/* </list> */}
+      </div>
+
+      <Button btnType={"New-product"}>New product</Button>
+
+      <Button btnType={"New-sales"}>New sales</Button>
+
+      <Button btnType={"Total-products"}>Total Products</Button>
+
+      <Button btnType={"Total-Sales"}>Total Sales</Button>
+    </Fragment>
+  );
+};
+export default Admin;
